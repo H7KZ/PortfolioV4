512bd4d402c20de406dda2f7b4a265801edeac28
H7KZ <74021016+H7KZ@users.noreply.github.com>
2024-09-05 19:22:58 +0200

    feat: new experience, new project

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -4,27 +4,16 @@
 		<div class="flex w-full max-w-[520px] flex-col gap-12">
 			<h2 class="font-inter text-5xl font-medium text-main">About me</h2>
 			<p class="lg:text-md font-inter text-sm text-main sm:text-base xl:text-lg">
-				<!-- My name is Jan Komínek, and I am a Fullstack Engineer with a keen interest in creating user-friendly and dynamic web applications. I graduated from
-				Smíchovská střední průmyslová škola a gymnázium, specializing in cybersecurity, with a full score in technical subjects. I am currently furthering my
-				education at the University of Economics and Business, Faculty of Informatics and Statistics.
-				<br /><br />
-				I am particularly skilled in SvelteKit and TailwindCSS and have extensive experience with TypeScript, VueJS, ReactJS, ExpressJS, and MongoDB. Additionally,
-				I have experience working with Raspberry Pi, hardware connections, and firmware development. I thrive in collaborative environments, working closely with
-				both technical and non-technical stakeholders to deliver outstanding results.
-				<br /><br />
-				As a Fullstack Engineer, I am committed to creating applications that are both technically excellent and user-friendly. I stay up-to-date with the latest
-				trends and best practices in my field and am always eager to take on new challenges and expand my skillset.
-				<br /><br />
-				My exceptional skills in Fullstack Engineering, combined with my experience in hardware and firmware, my ability to work collaboratively, and my commitment
-				to excellence, make me an ideal candidate for any team. If you are looking for a dynamic and talented Fullstack Engineer, I would love to hear from you! -->
-				I am Jan Komínek, a Fullstack Engineer passionate about developing user-friendly and dynamic web applications. I specialize in SvelteKit, TailwindCSS, and
-				have extensive experience with TypeScript, VueJS, ReactJS, ExpressJS, and MongoDB. With a background in cybersecurity from Smíchovská střední průmyslová
-				škola a gymnázium, where I excelled in technical subjects, I am now furthering my education at the University of Economics and Business, Faculty of Informatics
-				and Statistics. I also bring hands-on experience with Raspberry Pi, hardware connections, and firmware development.
+				I am Jan Komínek, a Fullstack Engineer passionate about developing user-friendly and dynamic web applications. I specialize in SvelteKit, TailwindCSS,
+				and have extensive experience with TypeScript, VueJS, ReactJS, ExpressJS, and MongoDB. With a background in cybersecurity from Smíchovská střední
+				průmyslová škola a gymnázium, where I excelled in technical subjects, I am now furthering my education at the University of Economics and Business,
+				Faculty of Informatics and Statistics. I also bring hands-on experience with Raspberry Pi, hardware connections, and firmware development.
 				<br /><br />
 				I thrive in collaborative environments, working closely with both technical and non-technical stakeholders to deliver high-quality results. My commitment
 				to creating technically excellent and user-friendly applications drives me to stay up-to-date with the latest industry trends and best practices. If you're
 				seeking a skilled and dynamic Fullstack Engineer, I am eager to bring my expertise to your team.
+				<br /><br />
+				<i>"Perfection is lots of little things done well"</i>
 			</p>
 		</div>
 	</div>
